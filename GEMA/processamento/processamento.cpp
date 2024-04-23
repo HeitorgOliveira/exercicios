dfca6c6a07a1f51157ebce1545051de5fda040f3
teste <email@teste.com>
2024-04-23 17:38:58 -0300

    tentei fazer o processamento (está com erros)

--- GEMA/processamento/processamento.cpp
+++ GEMA/processamento/processamento.cpp
@@ -1,41 +1,45 @@
 #include <iostream>
+#include <string>
 
 int main(void)
 {
-    int t, n, tamanhos[100];
+    int t, n;
     std::cin >> t;
-    std::string palavra, respostas[100];
+    //char palavra[] = "";
+    std::string resposta, palavra;
     for (int i = 0; i < t; i++)
     {
-        respostas[i] = "";
         std::cin >> n;
         std::cin >> palavra;
-        tamanhos[i] = n;
-        for (int j = 0; j < n; j)
+        for (int j = 0; j < n-1; j++)
         {
-            if((palavra[n-j] == 'a') || (palavra[n-j] == 'e'))
+            //printf("%c\n\n", palavra[n-j-1]);
+            if (palavra[n-j-1] == 'b' || palavra[n-j-1] == 'c' || palavra[n-j-1] == 'd')
             {
-                respostas[i] += '.' + palavra[n-j] + palavra[n-j-1];
+                //juntar letra de índice j e suas 2 anteriores em uma string de resposta
+                //aumentar o contador em 2
+                resposta += '.';
+                resposta += palavra[n-j-1];
+                resposta += palavra[n-j-2];
+                resposta += palavra[n-j-3];
                 j += 2;
             }
-            else if ((palavra[n-j] == 'b' || palavra[n-j] == 'c' || palavra[n-j] == 'd'))
+            if (palavra[n-j-1] == 'a'|| palavra[n-j-1] == 'e')
             {
-                respostas[i] += '.' + palavra[n-j] + palavra[n-j-1] + palavra[n-j-2];
-                j += 3;
-            }
-            else
-            {
-                printf(palavra[n-j]);
-                return 1;
+                //juntar letra de indice j e 1 anterior em uma string de resposta
+                //aumentar o contador em 1
+                resposta += '.';
+                resposta += palavra[n-j-1];
+                resposta += palavra[n-j-2];
+                j++;
             }
         }
-    }
-    for (int i = 0; i < t; i++)
-    {
-        for(int j = 0; j < tamanhos[i]-1; j++)
+        //ler string resposta de trás para frente ignorando o ultimo caracter ('.')
+        for (int j = sizeof(resposta)-1; j  > 0; j--)
         {
-            printf("%c", respostas[i][tamanhos[i]-j]);
+            printf("%c", resposta[j]);
         }
         printf("\n");
+        resposta = "";
     }
 }
