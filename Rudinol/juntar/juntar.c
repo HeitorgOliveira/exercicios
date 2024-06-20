f1a8e941032fe748346d95e227087ea136df6ebb
Heitor <heitor_portugal1@hotmail.com>
2024-06-19 23:12:30 -0300

    .

--- Rudinol/juntar/juntar.c
+++ Rudinol/juntar/juntar.c
@@ -1,31 +1,57 @@
 #include <stdio.h>
-#include <stdlib.h>
 #include <string.h>
 
-int main(void)
-{
-    char *A;
-    A = malloc(sizeof(char) * 131);
-    char *B;
-    B = malloc(sizeof(char) * 131);
-    char * palavra;
-    palavra = malloc(sizeof(char) * 20);
-    int cont = 0;
-    char *aux;
-    aux = malloc(sizeof(char) * 261);
-
-    A[sizeof(A)] = 0;
-    B[sizeof(B)] = 0;
+int main() {
+    char A[131], B[131], parametro[21];
+    char *posA, *posB;
 
     fgets(A, 131, stdin);
     fgets(B, 131, stdin);
-    fgets(A, 131, stdin);
-    strcpy(aux, A);
+    fgets(parametro, 21, stdin);
 
-    
+    A[strcspn(A, "\n")] = 0;
+    B[strcspn(B, "\n")] = 0;
+    parametro[strcspn(parametro, "\n")] = 0;
 
-    free(A);
-    free(B);
-    free(aux);
-    free(palavra);
+    posA = strstr(A, parametro);
+    posB = strstr(B, parametro);
+
+    int espaco_anterior = 0;
+
+    if (posA != NULL) {
+        for (char *p = A; p < posA; p++) {
+            if (*p != ' ' || (p > A && *(p - 1) != ' ')) {
+                putchar(*p);
+            }
+        }
+    } else {
+        for (char *p = A; *p != '\0'; p++) {
+            if (*p != ' ' || (p > A && *(p - 1) != ' ')) {
+                putchar(*p);
+            }
+        }
+    }
+
+    if (posB != NULL) {
+        posB += strlen(parametro);
+        while (*posB == ' ') {
+            posB++;
+        }
+
+        if (*(posB - strlen(parametro) - 1) == ' ') {
+            putchar(' ');
+        }
+
+        espaco_anterior = 1;
+        for (char *p = posB; *p != '\0'; p++) {
+            if (*p != ' ' || (espaco_anterior == 0)) {
+                putchar(*p);
+                espaco_anterior = (*p == ' ');
+            }
+        }
+    }
+
+    putchar('\n');
+
+    return 0;
 }
